9e699ee6263c2169ec785682d944c70cd7f44d8b
Suraj <surajshrm000@gmail.com>
2020-11-03 22:46:03 +0530

    [Suraj] Add: Ability to add Hotel

---
+++ HotelReservationSystem/Hotel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class Hotel
+    {
+        //Hotel Name
+        HotelType type;
+        //Rate for Regular Customer
+        public double RATE { get; }
+        public Hotel(HotelType hotelType)
+        {
+            this.type = hotelType;
+            try
+            {
+                if (hotelType.Equals(HotelType.LAKEWOOD))
+                {
+                    this.RATE = 110;
+                }
+                if (hotelType.Equals(HotelType.BRIDGEWOOD))
+                {
+                    this.RATE = 150;
+                }
+                if (hotelType.Equals(HotelType.RIDGEWOOD))
+                {
+                    this.RATE = 220;
+                }
+            }
+            catch (HotelReservationException)
+            {
+                throw new HotelReservationException(HotelReservationException.ExceptionType.INVALID_HOTEL_TYPE, "Invalid Hotel Type");
+            }
+        }
+    }
+}
---
+++ HotelReservationSystem/HotelType.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public enum HotelType { LAKEWOOD, BRIDGEWOOD, RIDGEWOOD }
+}
